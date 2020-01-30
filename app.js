5dda9d09427edd75aa2aefc18fafe9ec6d5791ff
Joshua Duke <dukejoshuae@gmail.com>
2020-01-30 11:38:42 -0500

    Adding styles to pages with bootstrap

--- app.js
+++ app.js
@@ -5,16 +5,19 @@ let bodyParser = require('body-parser');
 app.set("view engine", "ejs"); // to remove ejs extension from res.render
 app.use(bodyParser.urlencoded({extended:true}));
 
+let campgrounds = [
+  {name: "Star Labs", image: "https://vignette.wikia.nocookie.net/dccu/images/a/a4/S.T.A.R._Labs_-_Desk_of_Silas_Stone.jpg/revision/latest?cb=20180322215627"},
+  {name: "Batcave", image: "https://i.pinimg.com/originals/8a/49/1f/8a491fdd64f3f479a0b7576e1ba94b94.jpg"},
+  {name: "Fortress of solitude", image: "https://qph.fs.quoracdn.net/main-qimg-0c05c6a59d878179df5f1f529740b4fa"}, 
+   
+]
+
 app.get("/", (req, res)=>{
   res.render("landing");
 });
 
 app.get("/campgrounds", (req, res)=>{
-  let campgrounds = [
-      {name: "Salmon Creek", image: "https://media-cdn.tripadvisor.com/media/photo-s/0a/64/9d/47/salmon-creek-falls-trail.jpg"},
-      {name: "Batcave", image: "https://i.pinimg.com/originals/8a/49/1f/8a491fdd64f3f479a0b7576e1ba94b94.jpg"},
-      {name: "Fortress of solitude", image: "https://qph.fs.quoracdn.net/main-qimg-0c05c6a59d878179df5f1f529740b4fa"}  
-  ]
+
   res.render("campgrounds", {campgrounds: campgrounds}); //name: data passing in 
 });
 
@@ -24,8 +27,12 @@ app.get("/campgrounds/new", (req, res)=>{
 
 app.post("/campgrounds", (req,res)=>{
   //get data from form and add to campgrounds array
-res.send("active");
+  let name = req.body.name;
+  let image = req.body.image;
+  let newCampgrounds = {name: name, image: image}
+  campgrounds.push(newCampgrounds);
   //redirect back to campgrounds page
+  res.redirect("/campgrounds");
 });
 
 app.listen(3000 ,process.env.IP, ()=>{
