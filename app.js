1548b7376ec1f2c70f5f336c23d52fb44c68d34a
Joshua Duke <dukejoshuae@gmail.com>
2020-01-30 10:04:15 -0500

    Added body parser to project

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
 
 app.set("view engine", "ejs"); // to remove ejs extension from res.render
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", (req, res)=>{
   res.render("landing");
@@ -13,7 +15,17 @@ app.get("/campgrounds", (req, res)=>{
       {name: "Batcave", image: "https://i.pinimg.com/originals/8a/49/1f/8a491fdd64f3f479a0b7576e1ba94b94.jpg"},
       {name: "Fortress of solitude", image: "https://qph.fs.quoracdn.net/main-qimg-0c05c6a59d878179df5f1f529740b4fa"}  
   ]
-  res.render("campgrounds", {campgrounds: campgrounds}); //name: data passing in
+  res.render("campgrounds", {campgrounds: campgrounds}); //name: data passing in 
+});
+
+app.get("/campgrounds/new", (req, res)=>{
+  res.render("new");
+})
+
+app.post("/campgrounds", (req,res)=>{
+  //get data from form and add to campgrounds array
+res.send("active");
+  //redirect back to campgrounds page
 });
 
 app.listen(3000 ,process.env.IP, ()=>{
