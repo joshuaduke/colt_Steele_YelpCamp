825b11ac68df26adbd061a39f96d2e324a4664b1
Joshua Duke <dukejoshuae@gmail.com>
2020-02-11 09:50:32 -0500

    Added REST index and show

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-let express = require('express');
-let app = express();
-let bodyParser = require('body-parser');
+let express     = require('express'),
+    app         = express(),
+    bodyParser  = require('body-parser'),
+    mongoose    = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelpcamp", {useNewUrlParser: true});
 app.set("view engine", "ejs"); // to remove ejs extension from res.render
 app.use(bodyParser.urlencoded({extended:true}));
+
+//SCHEMA SETUP
+let campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String 
+});
+
+let Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({
+//     name: "Batcave",
+//     image: "https://i.pinimg.com/originals/8a/49/1f/8a491fdd64f3f479a0b7576e1ba94b94.jpg",
+//     description: "This is the batcave, Batman's secret hideout, only few know of it's existence"
+
+// }, 
+// function(err, campground){
+//   if(err){
+//     console.log(err);
+//   } else {
+//     console.log("NEWLY CREATED CAMPGROUND");
+//     console.log(campground)
+//   }
+// });
 
 let campgrounds = [
   {name: "Star Labs", image: "https://vignette.wikia.nocookie.net/dccu/images/a/a4/S.T.A.R._Labs_-_Desk_of_Silas_Stone.jpg/revision/latest?cb=20180322215627"},
@@ -16,24 +42,56 @@ app.get("/", (req, res)=>{
   res.render("landing");
 });
 
+//INDEX ROUTE
 app.get("/campgrounds", (req, res)=>{
-
-  res.render("campgrounds", {campgrounds: campgrounds}); //name: data passing in 
+  //Get all campgrounds from DB
+  Campground.find({}, function(err, allCampgrounds){
+    if(err){
+      console.log(err)
+    }else{
+      res.render("index", {campgrounds: allCampgrounds});
+    }
+  })
+  // res.render("campgrounds", {campgrounds: campgrounds}); //name: data passing in 
 });
 
+//NEW - Show form to create new campground
 app.get("/campgrounds/new", (req, res)=>{
   res.render("new");
 })
 
+// CREATE ROUTE - Add new campground to database
 app.post("/campgrounds", (req,res)=>{
   //get data from form and add to campgrounds array
   let name = req.body.name;
   let image = req.body.image;
-  let newCampgrounds = {name: name, image: image}
-  campgrounds.push(newCampgrounds);
-  //redirect back to campgrounds page
-  res.redirect("/campgrounds");
+  let description = req.body.description;
+  let newCampgrounds = {name: name, image: image, description: description};
+
+  //Create a new campground an save to database
+    // campgrounds.push(newCampgrounds);
+  Campground.create(newCampgrounds, function(err, newlyCreated){
+    if (err){
+      console.log(err);
+    }else{
+        //redirect back to campgrounds page
+      res.redirect("/campgrounds");
+    }
+  });
+
 });
+//SHOW - Shows more info about one campground
+app.get("/campgrounds/:id", (req,res)=>{
+  //find the campgroun qith provided id 
+  Campground.findById(req.params.id, function(err, foundCampground){
+    if(err){
+      console.log(err)
+    } else {
+        //render show template with that campground
+      res.render("show", {campground: foundCampground});
+    }
+  });
+})
 
 app.listen(3000 ,process.env.IP, ()=>{
   console.log("The server is running...");
